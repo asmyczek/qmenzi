c2ed11b705e905d80dd597f7038d5816a91cda7a
Adam Smyczek <adamsmyczek@gmail.com>
2024-05-02 19:33:57 +0200

    Cleaning up nlp lib

--- qmenzi/content/nlp.py
+++ qmenzi/content/nlp.py
@@ -5,7 +5,6 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from gensim.models.ldamodel import LdaModel
 from gensim import corpora
-from functools import reduce
 
 nltk.download('wordnet')
 nltk.download('stopwords')
@@ -17,37 +16,27 @@ EN_STOP = set(nltk.corpus.stopwords.words('english'))
 SENTIMENT_ANALYZER = SentimentIntensityAnalyzer()
 LEMMATIZER = WordNetLemmatizer()
 
-def tokenize(text):
-    lda_tokens = []
-    tokens = EN_PARSER(text)
-    for token in tokens:
-        if token.orth_.isspace():
-            continue
-        else:
-            lda_tokens.append(token.lower_)
-    return lda_tokens
 
-def cleanse_text(text):
-    tokens = tokenize(text)
+def tokenize(text):
+    tokens = EN_PARSER(text)
+    tokens = [token.lower_ for token in tokens if not token.orth_.isspace()]
     tokens = [token for token in tokens if len(token) > 4]
     tokens = [token for token in tokens if token not in EN_STOP]
     tokens = [LEMMATIZER.lemmatize(token) for token in tokens]
     return tokens
 
+
 def get_tokens(content):
-    return [cleanse_text(line) for line in content]
+    return [tokenize(line) for line in content]
 
-def get_topics(tokens):
+
+def get_topics(tokens, num_topics=3):
     dictionary = corpora.Dictionary(tokens)
-    corpus = [dictionary.doc2bow(text) for text in tokens]
+    corpus = [dictionary.doc2bow(token) for token in tokens]
 
-    lda = LdaModel(corpus, num_topics=3, id2word=dictionary, passes=15)
-    topics = lda.print_topics(num_words=4)
+    lda = LdaModel(corpus, num_topics=num_topics, id2word=dictionary, passes=15)
+    return [lda.show_topic(i) for i in range(num_topics)]
 
-    ret = []
-    for i, topic in topics:
-        ret.append(lda.show_topic(i))
-    return ret
 
 def get_sentiment(content):
     return [SENTIMENT_ANALYZER.polarity_scores(c) for c in content]
